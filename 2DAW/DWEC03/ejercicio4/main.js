601763cc78905d1061d24d0e9ef0d95ecd583402
AnNavNicolas <an.nav.nicolas@gmail.com>
2024-10-18 19:21:15 +0200

    ejercicio4 y correccion del ejercicio1

--- 2DAW/DWEC03/ejercicio4/main.js
+++ 2DAW/DWEC03/ejercicio4/main.js
@@ -1,7 +1,22 @@
-function comprobar() {
-    const textArea = document.getElementById("textArea").value
-    
-    
-
+function changeChar (texto, codificar) {
+    texto = texto.toLowerCase()
+    if(codificar){
+        document.getElementById("codificar").style.display = 'none'
+        document.getElementById("decodificar").style.display = 'inline'
+        if(/\d/.test(texto)){
+            window.alert("Error, no se pueden meter números")
+        }
+        document.getElementById("input").value = texto.replaceAll("l", "1").replaceAll(/e|é/gi, "3").replaceAll(/o|ó/gi, "0").replaceAll("s", "5")
+    } else if(!codificar){
+        document.getElementById("codificar").style.display = 'inline'
+        document.getElementById("decodificar").style.display = 'none'
+        document.getElementById("input").value = texto.replaceAll("1", "l").replaceAll("3", "e").replaceAll("0", "o").replaceAll("5", "s")
+    }
 }
-document.getElementById("codificar").addEventListener("click", comprobar)
+
+document.getElementById("codificar").addEventListener("click", function() {
+    changeChar(document.getElementById("input").value, true);
+});
+document.getElementById("decodificar").addEventListener("click", function() {
+    changeChar(document.getElementById("input").value, false);
+});
